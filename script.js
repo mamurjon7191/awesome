0968db1e9ba0a214866eb753352c5ce4e9b72199
mamurjon7191 <ibragimovmamurjon19@gmail.com>
2022-03-30 14:15:25 +0500

    alarm

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
 "use strict";
-let img1 = document.querySelector(".img1");
-let img2 = document.querySelector(".img2");
-let big = document.querySelector(".big");
+let btn = document.querySelector(".btn");
+let day = document.querySelector(".day");
+let hour = document.querySelector(".hour");
+let minute = document.querySelector(".minute");
+let second = document.querySelector(".second");
 
-big.addEventListener("mouseover", function (a) {
-  a.preventDefault();
-  if (a.target.classList.contains("img1")) {
-    img1.style.setProperty("--width1", 80);
-    img2.style.setProperty("--width2", 20);
-  } else if (a.target.classList.contains("img2")) {
-    img1.style.setProperty("--width1", 20);
-    img2.style.setProperty("--width2", 80);
-  }
+let kun = +day.value;
+let soat = +hour.value;
+let minut = +minute.value;
+let second1 = +second.value;
+
+btn.addEventListener("click", function () {
+  let kun = +day.value;
+  let soat = +hour.value;
+  let minut = +minute.value;
+  let second1 = +second.value;
+  let r = setInterval(function () {
+    // kun = +day.value;
+    // soat = +hour.value;
+    // minut = +minute.value;
+    // second1 = +second.value;
+
+    if (second1 <= 60) {
+      second.value = second1--;
+      if (second1 == -1) {
+        second1 = 60;
+        minute.value = --minut;
+        if (minut == 0) {
+          minut = 60;
+          hour.value = --soat;
+          if (soat == 0) {
+            day.value = --kun;
+            soat = 24;
+          } else if (soat == 0 && minut == 0) {
+            soat.value = 0;
+          }
+        }
+      }
+    }
+    // day.value = Number(kun--);
+    // hour.value = Number(soat--);
+    // minute.value = Number(minut--);
+    // second.value = Number(second1--);
+  }, 10);
 });
